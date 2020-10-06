3ba134e76f467268632bbcf805d13b22a96a4fe0
SerhiiNedohoda <sergnedohoda@gmail.com>
2020-10-06 22:42:30 +0300

    Calculating socks, hat and coast of things by color is in loop instead of functions for each operation

--- testTask.js
+++ testTask.js
@@ -21,55 +21,24 @@ let red = 0, green = 0, blue = 0;
 function getTotalPrice(inputGoods) {
 
     inputGoods.forEach((item) => {
-        totalPrice += parseInt((item.price || item.priceForPair).replace(/\$/, '')) * (item.quantity || 1);
+        totalPrice += parseInt((item.price || item.priceForPair).replace(/\$/, '')) * (item.quantity || 0);
     })
     return totalPrice;
 }
 
 console.log("Result of total price calculation: " + getTotalPrice(GOODS) + "$");
 
-function getPrice(inputObj) {
-    let price = 0;
-    price = inputObj.price != undefined ? parseInt(inputObj.price.replace(/\$/, '')) : parseInt(inputObj.priceForPair.replace(/\$/, ''));
+for (let i = 0; i < GOODS.length; i++) {
+    let item = GOODS[i];
 
-    return price;
+    if (item.type === "socks" && !!item.quantity) { socksQuantity += item.quantity};
+
+    if (item.type === "hat" && item.color === "red") { redHatQuantity += item.quantity}
+
+    if (item.color === "red") { red += parseInt((item.price || item.priceForPair).replace(/\$/, '')) * (item.quantity || 0)};
+    if (item.color === "green") { green += parseInt((item.price || item.priceForPair).replace(/\$/, '')) * (item.quantity || 0)};
+    if (item.color === "blue") { blue += parseInt((item.price || item.priceForPair).replace(/\$/, '')) * (item.quantity || 0)};
 }
 
-function calculateSocks(inputData) {
-
-    inputData.forEach((obj) => {
-        if (obj.type == "socks" && obj.quantity != undefined) {
-            socksQuantity = socksQuantity + obj.quantity
-            totalAmount = totalAmount + getPrice(obj) * obj.quantity;
-        };
-    });
-    
-    console.log("Socks - " + socksQuantity + ", Total price: " + totalAmount + "$");
-}
-
-function calculateRedHat(inputData) {
-    
-    inputData.forEach((obj) => {
-        if (obj.type == "hat" && obj.color == "red") { 
-            redHatQuantity = redHatQuantity + obj.quantity;
-            totalAmount = totalAmount + getPrice(obj) * obj.quantity;
-        };
-    });
-    
-    console.log("Red Hats - " + redHatQuantity + ", Total price: " + totalAmount + "$");
-}
-
-function calculateRGB(inputData) {
-    
-    inputData.forEach(function(obj) {
-        if (obj.color = "red" && obj.quantity != undefined) { red = red + getPrice(obj) * obj.quantity};
-        if (obj.color = "green" && obj.quantity != undefined) { green = green + getPrice(obj) * obj.quantity};
-        if (obj.color = "blue" && obj.quantity != undefined) { blue = blue + getPrice(obj) * obj.quantity};
-    });
-    
-    console.log("Red - " + red + "$, Green - " + green + "$, Blue - " + blue + "$");
-}
-
-calculateSocks(input);
-calculateRedHat(input);
-calculateRGB(input);
+console.log("Socks - " + socksQuantity + " pairs\nRed Hats - " + redHatQuantity + 
+            "\nRed - " + red + "$, Green - " + green + "$, Blue - " + blue + "$");
