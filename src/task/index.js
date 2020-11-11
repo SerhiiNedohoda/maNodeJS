747382c8ff789ce98d25a7fddba6ea948bb3334b
SerhiiNedohoda <sergnedohoda@gmail.com>
2020-11-11 21:23:40 +0200

    export task1, task2, task3 in index.js without declaring object

--- src/task/index.js
+++ src/task/index.js
@@ -3,10 +3,8 @@ const task1 = require('./task1');
 const task2 = require('./task2');
 const task3 = require('./task3');
 
-const tasks = {
+module.exports.tasks = {
     task1: task1,
     task2: task2,
     task3: task3,
 };
-
-module.exports.tasks = tasks;
