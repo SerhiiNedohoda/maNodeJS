3c20e45c169b107bf07044b2fd17f978221abcc8
SerhiiNedohoda <sergnedohoda@gmail.com>
2020-11-15 23:31:26 +0200

    Add rest.http and server handler in app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,35 @@
-const { task1: firstTask, task2: secondTask, task3 } = require('./task');
-const GOODS = require('../goods.json');
+require('dotenv').config();
+const http = require('http');
 
-function boot(arr, key, value) {
-    const resultFirstTask = firstTask(arr, key, value);
+console.log(`${process.env.ORIGIN}:${process.env.PORT}`);
 
-    console.log(resultFirstTask);
-    console.log(task3(resultFirstTask));
-    console.log(secondTask);
+const requestHandler = (request, response) => {
+    console.log('call Handler')
+    if (request.url === '/ping') {
+        console.log(`Request url is: ${request.url}`);        
+    }
+    response.end('pong')
 }
 
-boot(GOODS, 'type', 'socks');
+const server = http.createServer(requestHandler);
+
+server.listen(process.env.PORT, (err) => {
+    if (err) {
+        return console.log(`Someting went wrong, ${err}`)
+    }
+
+    console.log(`Server is listening on port: ${process.env.PORT}`)
+})
+
+// const { task1: firstTask, task2: secondTask, task3 } = require('./task');
+// const GOODS = require('../goods.json');
+
+// function boot(arr, key, value) {
+//     const resultFirstTask = firstTask(arr, key, value);
+
+//     console.log(resultFirstTask);
+//     console.log(task3(resultFirstTask));
+//     console.log(secondTask);
+// }
+
+// boot(GOODS, 'type', 'socks');
