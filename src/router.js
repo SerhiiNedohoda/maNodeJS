fd0b75385206f1b906131741805cfd57a451dd7e
SerhiiNedohoda <sergnedohoda@gmail.com>
2020-11-18 14:25:41 +0200

    fix switch-case fot router

--- src/router.js
+++ src/router.js
@@ -17,26 +17,36 @@ module.exports = (request, response) => {
         checkingGoods,
     } = request;
 
-    switch (method && pathname) {
-        case 'GET' && '/ping':
-            return ping(response);
+    if (method === 'GET') {
+        switch (pathname) {
+            case '/ping':
+                return ping(response);
 
-        case 'GET' && '/task1':
-            return controllers.firstTask(checkingGoods, response, queryParams);
+            case '/task1':
+                return controllers.firstTask(checkingGoods, response, queryParams);
 
-        case 'GET' && '/task2':
-            return controllers.secondTask(response);
+            case '/task2':
+                return controllers.secondTask(response);
 
-        case 'GET' && '/task3':
-            return controllers.thirdTask(checkingGoods, response);
+            case '/task3':
+                return controllers.thirdTask(checkingGoods, response);
 
-        case 'POST' && '/add-goods':
-            return controllers.newGoods(data, response);
-
-        case 'POST' && '/source':
-            return controllers.addSource(data, response);
-
-        default:
-            return notFound(response);
+            default:
+                break;
+        }
     }
+
+    if (method === 'POST') {
+        switch (pathname) {
+            case '/add-goods':
+                return controllers.newGoods(data, response);
+
+            case '/source':
+                return controllers.addSource(data, response);
+
+            default:
+                break;
+        }
+    }
+    return notFound(response);
 };
