f0b8f60296a078a1970a86f08a832f0637e41bea
SerhiiNedohoda <sergnedohoda@gmail.com>
2020-11-23 02:46:22 +0200

    update myMap function and add create discount function

--- src/checkSource.js
+++ src/checkSource.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-extend-native */
+/* eslint-disable consistent-return */
+require('dotenv').config();
 const GOODS = require('../goods.json');
 
 module.exports = function sourceCheck(source, store) {
@@ -5,13 +8,12 @@ module.exports = function sourceCheck(source, store) {
     return GOODS;
 };
 
-// eslint-disable-next-line no-extend-native
-Array.prototype.myMap = (callback) => {
-    const modifiedArr2 = [];
+Array.prototype.myMap = function (callback) {
+    const modifiedArr = [];
 
-    for (let i = 0; i < this.length; i += 1) modifiedArr2.push(callback(this[i]));
+    for (let i = 0; i < this.length; i += 1) modifiedArr.push(callback(this[i]));
 
-    return modifiedArr2;
+    return modifiedArr;
 };
 
 const resultArray = GOODS.myMap((param) => {
@@ -27,3 +29,39 @@ const resultArray = GOODS.myMap((param) => {
 });
 
 console.log(resultArray);
+
+// Створити функцію з колбеком для генерування знижки, яка поверне свій результат в колбек через 50мс.
+// Згенерувати знижку випадковим чином в діапазоні від 1 до 99%. Оскільки магазин не може собі дозволити
+// великі знижки, то якщо знижка буде більше 20 (включно), функція поверне помилку,
+// інакше поверне значення знижки. Створити обгортки над функцією з колбеком для полегшення роботи з
+// асинхронними функціями. Обгортки мають бути реалізовані двома способами.
+
+// function getDiscount(callback) {
+//     let random = 0;
+//     do {
+//         random = callback(1, 100);
+//     } while (random >= process.env.MAX_DISCOUNT);
+//     return random;
+// }
+
+function discount(callback) {
+    const min = Math.ceil(1);
+    const max = Math.floor(100);
+    const res = Math.floor(Math.random() * (max - min)) + min;
+    if (res >= 20) {
+        callback(new Error('Discount is too big'));
+        return;
+    }
+    return callback(null, res);
+}
+
+const callback = (error, result) => {
+    if (error !== null) {
+        console.log(`Get error: ${error}`);
+        return;
+    }
+    console.log(`Your discount: ${result}`);
+    return result;
+};
+
+discount(callback);
